ed974ac56f82277d2f8fd9174840955080036e2d
ereminmaksim <eremin1988ma21@gmail.com>
2022-03-02 13:27:51 +0300

    проверка task_1

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import styles from './Message.module.css'
+
+
+interface MessageProps {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+
+function Message(props: MessageProps) {
+    const {avatar, name, message, time} = props
+
+    return (
+        <div>
+            <h1>Отображение формы отправки</h1>
+            <div className={styles.head}>
+                <div className={styles.wrapper}>
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'end',
+                        marginRight: '9px'
+                    }}>
+                        <img className={styles.ava} src={avatar} alt="avatar"/>
+                    </div>
+                    <div className={styles.blockMessage}>
+                        <h2>{name}</h2>
+                        <span>{message}</span>
+                        <span className={styles.inTime}>{time}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Message
